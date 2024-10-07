dc7dfd5cb44add8787237e9a22fd670894e57ba0
Atabur Rahaman Mollah <atabur8119921@gmail.com>
2024-10-07 23:48:44 +0530

    Added signup component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
+import "./App.css";
 
-import Navbar from './components/Navbar'
-import TextForm from './components/TextForm';
+import Navbar from "./components/Navbar";
+import Signup from "./components/Signup";
+// import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
-      <Navbar title={'E-Doc'} home={'Home'} link={'Link'} />
-      <TextForm heading={'Write Your Document:'} />
+      <Navbar title={"E-Doc"} home={"Home"} link={"Link"} />
+      {/* <TextForm heading={'Write Your Document:'} /> */}
+
+      <Signup></Signup>
     </>
   );
 }
